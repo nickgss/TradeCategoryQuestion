dfc436d16f896fdd7334dd5f5888fa2a3a4e7221
Geder Severino <gederseverino@tokenlab.com.br>
2022-05-23 09:02:58 -0300

    Base Creation

---
+++ TradeCategory/TradeCategoryQuestion/Interfaces/ITrade.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TradeCategoryQuestion.Interfaces
+{
+    public interface ITrade
+    {
+        double Value { get; } // Indicates the transaction amount in dollars
+        string ClientSector { get; } // Indicates the client's sector which can be "Public" or "Private"
+        DateTime NextPaymentDate { get; } //Indicates when the next payment from the client to the bank is expected
+    }
+}
---
+++ TradeCategory/TradeCategoryQuestion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TradeCategoryQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
